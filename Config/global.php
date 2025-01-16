f27a5314a609c965a36340f8ad1fd33370d41a16
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-01-15 21:02:03 -0500

    ya no llevo la chingada perros

--- Config/global.php
+++ Config/global.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'db_elegans');
+define('DB_NAME', '');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '1234');
+define('DB_PASSWORD', '');
 define('DB_ENCODE', 'utf8');
